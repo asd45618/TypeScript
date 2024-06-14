ee78f54fbc8350bcf755acd337c199a1d1800577
kwang99 <asd45618@naver.com>
2024-06-14 18:36:20 +0900

    타입스크립트 공부

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Circle from "./Circle";
+
+import styled from "styled-components";
+
+// import { useState } from "react";
 
 function App() {
+  // const [item, setItem] = useState("");
+  // const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+  //   const {
+  //     currentTarget: { value },
+  //   } = e;
+  //   setItem(value);
+  // };
+
+  // const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   console.log("hello", item);
+  // };
+
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `;
+
+  const H1 = styled.div`
+    color: ${(props) => props.theme.textColor};
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Circle borderColor="yellow" bgColor="teal" /> */}
+      {/* <Circle text="im here" bgColor="tomato" /> */}
+
+      {/* <form onSubmit={onSubmit}>
+        <input
+          value={item}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form> */}
+      <Container>
+        <H1>protected</H1>
+      </Container>
     </div>
   );
 }
